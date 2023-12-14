fe421532596f448fbd2fe39a7ab78d1745367109
zhang yue <yuedev.cn@gmail.com>
2023-12-14 16:42:11 +0800

    add some composable functions.

--- app/src/main/java/com/example/newcollage/compose/ComposeActivity.kt
+++ app/src/main/java/com/example/newcollage/compose/ComposeActivity.kt
@@ -3,44 +3,56 @@ package com.example.newcollage.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.newcollage.compose.ui.theme.ComposeTestNewTheme
+import androidx.compose.ui.unit.dp
+import com.example.newcollage.R
+import com.example.newcollage.compose.ui.theme.ComposeTheme
 
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTestNewTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(Message("Android", "Jetpack Compose"))
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.test_2),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Contact profile picture",
+            modifier = Modifier.size(96.dp)
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ComposeTestNewTheme {
-        Greeting("Android")
-    }
+fun MessageCardPreview() {
+    MessageCard(
+        msg = Message("Lexi", "Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
+
+data class Message(val author: String, val body: String)
--- app/src/main/java/com/example/newcollage/compose/ui/theme/Theme.kt
+++ app/src/main/java/com/example/newcollage/compose/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun ComposeTestNewTheme(
+fun ComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
