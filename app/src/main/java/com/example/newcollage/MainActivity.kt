a3745b9c797d4634e28f9f900bbb26992d554007
zhang yue <yuedev.cn@gmail.com>
2023-12-26 21:24:42 +0800

    commit.

--- app/src/main/java/com/example/newcollage/MainActivity.kt
+++ app/src/main/java/com/example/newcollage/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val items = ImageRepository.imageResIds.shuffled().map {res ->
+        val items = ImageRepository.imageResIds.shuffled().map { res ->
             val bitmap = BitmapFactory.decodeResource(resources, res)
             CollageItem(bitmap)
         }
