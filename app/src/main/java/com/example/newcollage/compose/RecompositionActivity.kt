16d00d92b4093d0f00b9027d253307e84858ebd5
zhang yue <yuedev.cn@gmail.com>
2024-02-28 09:55:45 +0800

    add RecompositionTest.

--- app/src/main/java/com/example/newcollage/compose/RecompositionActivity.kt
+++ app/src/main/java/com/example/newcollage/compose/RecompositionActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.newcollage.compose.ui.theme.NewCollageTheme
@@ -29,11 +37,29 @@ class RecompositionActivity : ComponentActivity() {
 
 @Composable
 private fun RecompositionScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Recomposition",
-        modifier = modifier
-    )
+    var num by remember { mutableIntStateOf(0) }
+
+    // Column是内联函数，直接用Column会导致RecompositionScreen重组
+    // 写一个不内联的MyColumn 可以再点击button的时候让RecompositionScreen不重组
+    // 但我感觉意义不大，因为这里num用by remember记住了 所以直接用Column就行
+    MyColumn(modifier = modifier) {
+        Text(text = "RecompositionTest")
+        Text(text = "Click num: $num")
+        Button(onClick = { num++ }) {
+            Text(text = "Click")
+        }
+
+    }
 }
+
+@Composable
+private fun MyColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Column(modifier = modifier, content = content)
+}
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/newcollage/compose/SegmentActivity.kt
+++ app/src/main/java/com/example/newcollage/compose/SegmentActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NonRestartableComposable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -52,13 +53,11 @@ class SegmentActivity : ComponentActivity() {
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(innerPadding)
-//                        viewModel = viewModel
                     )
                 }
             }
         }
     }
-
 }
 
 @Composable
